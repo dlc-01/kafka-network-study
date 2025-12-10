f276beabe3b81ed47871208ad89590c79be4a103
DLC <shabrov23.02@gmail.com>
2025-12-10 15:08:59 +0300

    codecrafters submit [skip ci]

--- internal/domain/response.go
+++ internal/domain/response.go
@@ -1,9 +1,8 @@
 package domain
 
 type MessageResponse struct {
-	CorrelationID           uint32
-	ErrorCode               uint16
-	ApiKeys                 []ApiKey
-	DescribeTopicPartitions DescribeTopicPartitions
-	ThrottleTime            uint32
+	CorrelationID uint32
+	ErrorCode     uint16
+	ApiKeys       []ApiKey
+	ThrottleTime  uint32
 }
